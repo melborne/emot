c5099c7835915fa759717b64f6237bdfd0a33ef0
kyoendo <postagie@gmail.com>
2014-05-17 09:33:17 +0900

    implement icon, unicode, list methods

--- lib/emojit.rb
+++ lib/emojit.rb
@@ -1,5 +1,37 @@
 require "emojit/version"
+require "emojit/map"
 
 module Emojit
-  # Your code goes here...
+  def icon(name)
+    if codes = MAP[name.intern]
+      build_icon(codes)
+    else
+      nil
+    end
+  end
+
+  def unicode(name)
+    if codes = MAP[name.intern]
+      build_unicode(codes)
+    else
+      nil
+    end
+  end
+
+  def list
+    MAP.map do |name, codes|
+      [name, [build_icon(codes), build_unicode(codes)]]
+    end.sort_by(&:last).to_h
+  end
+
+  private
+  def build_icon(codes)
+    codes.map { |code| code.to_i(16) }.pack("U*")
+  end
+
+  def build_unicode(codes)
+    codes.map { |code| "U+#{code}" }.join(" ")
+  end
+
+  extend self
 end
