b09ded341dce233d706e2ebbe7bf7a31f539bd1f
kyoendo <postagie@gmail.com>
2014-05-17 09:32:54 +0900

    add examples for icon, unicode, list methods

--- spec/emojit_spec.rb
+++ spec/emojit_spec.rb
@@ -5,7 +5,28 @@ describe Emojit do
     expect(Emojit::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe ".icon" do
+    it "returns icon for given name" do
+      expect(Emojit.icon :apple).to eq "\u{1F34E}"
+      expect(Emojit.icon 'airplane').to eq "\u{2708}\u{FE0F}"
+      expect(Emojit.icon :hello_world).to be_nil
+    end
+  end
+
+  describe ".unicode" do
+    it "returns unicode in string" do
+      expect(Emojit.unicode 'apple').to eq "U+1F34E"
+      expect(Emojit.unicode :airplane).to eq "U+2708 U+FE0F"
+      expect(Emojit.unicode :hello_world).to be_nil
+    end
+  end
+
+  describe ".list" do
+    it "returns emoji list" do
+      list = Emojit.list
+      expect(list[:"+1"]).to eq ["\u{1F44D}", "U+1F44D"]
+      expect(list[:airplane]).to eq ["\u{2708}\u{FE0F}", "U+2708 U+FE0F"]
+      expect(list.first).to eq [:hash, ["\u{23}\u{FE0F}\u{20E3}", "U+23 U+FE0F U+20E3"]]
+    end
   end
 end
