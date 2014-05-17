9eef435b5b5fc79f07127beaf0f8b180eb42a1f8
kyoendo <postagie@gmail.com>
2014-05-17 17:09:54 +0900

    add spec for CLI

---
+++ spec/cli_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe Emojit::CLI do
+  before do
+    $stdout, $stderr = StringIO.new, StringIO.new
+  end
+
+  after() do
+    $stdout, $stderr = STDOUT, STDERR
+  end
+
+  describe "#show" do
+    context "with emoji name" do
+      it "outputs icon with its unicode" do
+        Emojit::CLI.start(['show', 'sunflower'])
+        expect($stdout.string).to eq "\u{1F33B} U+1F33B"
+      end
+    end
+  end
+end
