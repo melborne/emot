2d79e5bdf800726fa25e005381453821fb14d461
kyoendo <postagie@gmail.com>
2014-05-17 17:47:37 +0900

    implement show option --only

--- lib/emojit/cli.rb
+++ lib/emojit/cli.rb
@@ -3,10 +3,24 @@ require "thor"
 module Emojit
   class CLI < Thor
     desc "show [NAME]", "show emoji icon and unicode"
+    option :only
     def show(name=nil)
       case name
-      when nil
-        "not implement"
+      when nil, 'all'
+        list =
+          Emojit.list.map do |name, (icon, code)|
+            case options[:only]
+            when 'name'
+              "%s: %s" % [icon, name]
+            when 'code', 'unicode'
+              "%s: %s" % [icon, code]
+            when 'emoji'
+              "%s" % [icon]
+            else
+              "%s: %s (%s)" % [icon ,name, code]
+            end
+          end
+        puts list.join("  ")
       else
         print "%s %s\n" % Emojit.list[name.intern]
       end
