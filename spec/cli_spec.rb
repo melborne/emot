e75491c2497cb056af40ae0d260b30a448ec641d
kyoendo <postagie@gmail.com>
2014-05-17 17:46:51 +0900

    add examples for show command

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -16,5 +16,18 @@ describe Emojit::CLI do
         expect($stdout.string).to eq "\u{1F33B} U+1F33B\n"
       end
     end
+
+    context "without emoji name" do
+      it "outputs all emoji with name and unicode" do
+        Emojit::CLI.start(['show'])
+        expect($stdout.string).to match /hash.*sunflower.*1F618.*bathtub/
+      end
+
+      it "outputs all emoji with name" do
+        Emojit::CLI.start(['show', '--only', 'name'])
+        expect($stdout.string).to match /hash.*sunflower.*bathtub/
+        expect($stdout.string).not_to match /1F618/
+      end
+    end
   end
 end
